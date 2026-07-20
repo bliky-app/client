996e720ca736a67765d414fc83e47a42b78a787e
Kirill Volychev <volychevk@gmail.com>
2026-07-21 01:17:12 +0300

    fix(mock): compute completed appointments dynamically based on current time

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.snr6d6s9tr8"
+    "revision": "0.p20r75b785"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
